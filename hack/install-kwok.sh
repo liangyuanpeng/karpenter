34978ae13368270edccc9b46eee41bd27ede681d
Lan Liang <gcslyp@gmail.com>
2024-08-12 09:02:00 +0000

    u

--- hack/install-kwok.sh
+++ hack/install-kwok.sh
@@ -117,6 +117,6 @@ then
 else
   kubectl apply -f ${HOME_DIR}/kwok.yaml
   kubectl apply -f ${crdURL}
-  kubectl scale -n kube-system deploy/kwok-controller-a --replicas 0
+  # kubectl scale -n kube-system deploy/kwok-controller-a --replicas 0
 fi
 
